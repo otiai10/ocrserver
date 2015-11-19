8265b80ae2a9de4762381e92868b40bcf914ebde
otiai10 <otiai10@gmail.com>
2015-11-20 02:54:26 +0900

    Fix sample page a bit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 "use strict"; // ES6
 window.onload = () => {
-  // utils
   var http = {
     post: (path, data) => {
       return new Promise((resolve, reject) => {
@@ -18,23 +17,18 @@ window.onload = () => {
   var render = {
     response: document.getElementById("response"),
     success: (res) => {
-      render.response.className = "ok";
-      render.response.innerHTML = JSON.parse(res).result;
+      render.response.innerHTML = JSON.stringify(JSON.parse(res), null, 2);
     },
     error: (res) => {
-      render.response.className = "ng";
-      render.response.innerHTML = JSON.parse(res).error;
+      render.response.innerHTML = JSON.stringify(JSON.parse(res), null, 2);
     }
   };
-
-  // file
   document.getElementById("file-submit").addEventListener("click", () => {
     var data = new FormData();
     var files = document.getElementById("file").files;
     if (files.length != 0) data.append("file", files[0]);
     http.post("/file", data).then(render.success).catch(render.error);
   });
-  // base64
   document.getElementById("base64-submit").addEventListener("click", () => {
     var data = {base64: document.getElementById("base64").value};
     http.post("/base64", JSON.stringify(data)).then(render.success).catch(render.error);
